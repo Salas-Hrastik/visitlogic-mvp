2fef19dd6df461da12b0903c3cac9eb378c800dc
Salas-Hrastik <druzic@bak.hr>
2026-02-15 17:37:54 +0100

    Update valpovoData.js

--- data/valpovoData.js
+++ data/valpovoData.js
@@ -1,1 +1,55 @@
+const valpovoData = {
 
+  znamenitosti: [
+    {
+      name: "Dvorac Prandau-Normann",
+      description: "Barokni dvorac iz 18. stoljeća s perivojem.",
+      link: "https://tz.valpovo.hr"
+    },
+    {
+      name: "Muzej Valpovštine",
+      description: "Muzej smješten u dvorcu s povijesnom zbirkom.",
+      link: "https://tz.valpovo.hr"
+    },
+    {
+      name: "Crkva sv. Roka",
+      description: "Povijesna crkva iz 18. stoljeća u centru grada.",
+      link: "https://tz.valpovo.hr"
+    }
+  ],
+
+  priroda: [
+    {
+      name: "Perivoj dvorca",
+      description: "Uređeni park oko dvorca idealan za šetnju.",
+      link: "https://tz.valpovo.hr"
+    }
+  ],
+
+  gastronomija: [
+    {
+      name: "Restoran Park",
+      description: "Restoran s tradicionalnom slavonskom kuhinjom.",
+      link: "https://tz.valpovo.hr"
+    }
+  ],
+
+  smještaj: [
+    {
+      name: "Pansion Valpovo",
+      description: "Privatni smještaj u centru grada.",
+      link: "https://tz.valpovo.hr"
+    }
+  ],
+
+  događanja: [
+    {
+      name: "Valpovačko ljeto",
+      description: "Kulturno-zabavna manifestacija tijekom ljeta.",
+      link: "https://tz.valpovo.hr"
+    }
+  ]
+
+};
+
+export default valpovoData;
