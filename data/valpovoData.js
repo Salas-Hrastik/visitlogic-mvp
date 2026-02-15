eb717d06c786ca7e39ef73401cedfe5cd5393f68
Salas-Hrastik <druzic@bak.hr>
2026-02-15 17:58:24 +0100

    Update valpovoData.js

--- data/valpovoData.js
+++ data/valpovoData.js
@@ -1,53 +1,320 @@
 const valpovoData = {
 
-  znamenitosti: [
-    {
-      name: "Dvorac Prandau-Normann",
-      description: "Barokni dvorac iz 18. stoljeća s perivojem.",
-      link: "https://tz.valpovo.hr"
-    },
-    {
-      name: "Muzej Valpovštine",
-      description: "Muzej smješten u dvorcu s povijesnom zbirkom.",
-      link: "https://tz.valpovo.hr"
-    },
-    {
-      name: "Crkva sv. Roka",
-      description: "Povijesna crkva iz 18. stoljeća u centru grada.",
-      link: "https://tz.valpovo.hr"
-    }
-  ],
-
-  priroda: [
-    {
-      name: "Perivoj dvorca",
-      description: "Uređeni park oko dvorca idealan za šetnju.",
-      link: "https://tz.valpovo.hr"
-    }
-  ],
-
-  gastronomija: [
-    {
-      name: "Restoran Park",
-      description: "Restoran s tradicionalnom slavonskom kuhinjom.",
-      link: "https://tz.valpovo.hr"
-    }
-  ],
+  /* =========================
+     SMJEŠTAJ
+     ========================= */
 
   smještaj: [
+
     {
-      name: "Pansion Valpovo",
-      description: "Privatni smještaj u centru grada.",
-      link: "https://tz.valpovo.hr"
+      name: "Villa Valpovo",
+      type: "Hotel",
+      category: "4 zvjezdice",
+      address: "Bana J. Jelačića 1, Valpovo"
+    },
+
+    {
+      name: "Valpovački Dvori",
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "Matije Gupca 95, Valpovo"
+    },
+
+    {
+      name: 'Studio Apartman "M"',
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "Kralja Petra Krešimira IV 6, Valpovo"
+    },
+
+    {
+      name: 'Studio Apartman "Baky"',
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "Ivana Gorana Kovačića 31, Valpovo"
+    },
+
+    {
+      name: 'Sobe za iznajmljivanje "Čičak"',
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "B.I. Mažuranića 10, Valpovo"
+    },
+
+    {
+      name: 'Sobe "Room D"',
+      type: "Soba za iznajmljivanje",
+      category: "3 zvjezdice",
+      address: "Ulica Bana Ivana Mažuranića 10, Valpovo"
+    },
+
+    {
+      name: 'Soba za iznajmljivanje "Draft Room"',
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "Dr. Franje Tuđmana 3, Valpovo"
+    },
+
+    {
+      name: 'Soba "VaLux"',
+      type: "Objekt u domaćinstvu",
+      category: "4 zvjezdice",
+      address: "Dobriše Cesarića 58, Valpovo"
+    },
+
+    {
+      name: "Prenoćište Setnik",
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "Osječka 48, Valpovo"
+    },
+
+    {
+      name: "Olive Holiday House",
+      type: "Objekt u domaćinstvu",
+      category: "5 zvjezdica",
+      address: "Matije Gupca 22, Harkanovci"
+    },
+
+    {
+      name: "Business Club",
+      type: "Objekt u domaćinstvu",
+      category: "4 zvjezdice",
+      address: "Sunčana 26, Valpovo"
+    },
+
+    {
+      name: "Apartman Nives i Apartman Lea",
+      type: "Objekt u domaćinstvu",
+      category: "3/4 zvjezdice",
+      address: "Ivana Gorana Kovačića 25, Valpovo"
+    },
+
+    {
+      name: "Apartman Centar",
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "Osječka ulica 6, Valpovo"
+    },
+
+    {
+      name: "Apartman Ana 1",
+      type: "Objekt u domaćinstvu",
+      category: "4 zvjezdice",
+      address: "Ivana Gundulića 23, Valpovo"
+    },
+
+    {
+      name: 'Apartman "Tea"',
+      type: "Objekt u domaćinstvu",
+      category: "4 zvjezdice",
+      address: "Učka 57, Valpovo"
+    },
+
+    {
+      name: "Privatni smještaj Tolić",
+      type: "Objekt u domaćinstvu",
+      category: "3 zvjezdice",
+      address: "Vladimira Nazora 2, Ladimirevci"
+    },
+
+    {
+      name: "M&S – Sobe za iznajmljivanje",
+      type: "Objekt u domaćinstvu",
+      category: "4 zvjezdice",
+      address: "Trg kralja Tomislava 6, Valpovo"
     }
+
   ],
 
-  događanja: [
+
+  /* =========================
+     GASTRONOMIJA
+     ========================= */
+
+  gastronomija: [
+
+    /* RESTORANI */
+
     {
-      name: "Valpovačko ljeto",
-      description: "Kulturno-zabavna manifestacija tijekom ljeta.",
-      link: "https://tz.valpovo.hr"
+      name: "PIGment",
+      type: "Restoran",
+      address: "Trg kralja Tomislava 7",
+      rating: "5.0 (29 recenzija)",
+      description: "Burgeri, pulled pork"
+    },
+
+    {
+      name: "Restoran Jovalija",
+      type: "Restoran",
+      address: "Ul. Ive Lole Ribara 1",
+      rating: "4,9 (48)",
+      description: "Klasični restoran"
+    },
+
+    {
+      name: "Hotel & Restoran Park",
+      type: "Restoran",
+      address: "Ul. Ive Lole Ribara 10",
+      rating: "4,7 (1403)",
+      description: "Roštilj, riba"
+    },
+
+    {
+      name: "RasoPaso",
+      type: "Restoran",
+      address: "Strossmayerova ul. 2",
+      rating: "4,4 (52)"
+    },
+
+    {
+      name: "Gurman",
+      type: "Restoran",
+      address: "Ul. Augusta Šenoe 107",
+      rating: "4,0 (143)",
+      description: "Pizze, ćevapi, dostava"
+    },
+
+    /* PIZZERIA */
+
+    {
+      name: "Caffe Pizzeria HEX",
+      type: "Pizzeria",
+      address: "Trg kralja Tomislava 18",
+      rating: "4,7 (364)",
+      description: "Pizze, sladoled, kolači"
+    },
+
+    /* BRZA HRANA */
+
+    {
+      name: "Fast Food Vespera",
+      type: "Brza hrana",
+      address: "Ul. Zrinsko Frankopanska 48",
+      rating: "4,9 (118)",
+      description: "Burgeri, hot dog, sendviči"
+    },
+
+    {
+      name: "Fast Food Lana",
+      type: "Brza hrana",
+      address: "Osječka ul. 14",
+      rating: "4,0 (58)",
+      description: "Ćevapi"
+    },
+
+    /* CAFFE BAROVI */
+
+    {
+      name: "Caffe Bar Kolaž",
+      type: "Caffe bar",
+      address: "Trg kralja Tomislava 16",
+      rating: "4,8 (122)"
+    },
+
+    {
+      name: "Caffe Bar Best",
+      type: "Caffe bar",
+      address: "Trg kralja Tomislava",
+      rating: "4,7 (362)"
+    },
+
+    {
+      name: "Caffe Bar Prandau",
+      type: "Caffe bar",
+      address: "Ul. Matije Gupca 27",
+      rating: "4,8 (67)"
+    },
+
+    {
+      name: "Caffe Bar Vespera",
+      type: "Caffe bar",
+      address: "Ul. Zrinsko Frankopanska 48",
+      rating: "4,7 (105)"
+    },
+
+    {
+      name: "Cafe Barba",
+      type: "Caffe bar",
+      address: "Osječka ul. 8",
+      rating: "4,7 (109)"
+    },
+
+    {
+      name: "Caffe Bar House",
+      type: "Caffe bar",
+      address: "Ul. Braće Radić",
+      rating: "4,7 (17)"
+    },
+
+    {
+      name: "Baza",
+      type: "Caffe bar",
+      address: "Ul. kralja Petra Krešimira IV 1",
+      rating: "4.6 (276)",
+      description: "Rock atmosfera, glazba uživo"
+    },
+
+    {
+      name: "Gradska kavana Katančić",
+      type: "Kavana",
+      address: "Trg kralja Tomislava 2",
+      rating: "4.4 (88)"
+    },
+
+    {
+      name: "Caffe Bar Space",
+      type: "Pub",
+      address: "Ul. Braće Radić 9",
+      rating: "4,0 (67)"
+    },
+
+    /* NOĆNI BAROVI */
+
+    {
+      name: "Caffe Bar Passage",
+      type: "Noćni bar",
+      address: "Trg kralja Tomislava 16",
+      rating: "4.6 (317)"
+    },
+
+    {
+      name: "Caffe Night Bar City",
+      type: "Noćni bar",
+      address: "Ul. bana Ivana Mažuranića 8",
+      rating: "4,5 (98)"
+    },
+
+    {
+      name: "Cafe Bar Blue",
+      type: "Noćni bar",
+      address: "Trg kralja Tomislava 17-18",
+      rating: "4.3 (113)"
+    },
+
+    {
+      name: "Barcode Night Bar",
+      type: "Klub",
+      address: "Osječka ul. 14",
+      rating: "4.1 (103)"
+    },
+
+    /* PEKARE */
+
+    {
+      name: "Čičak d.o.o.",
+      type: "Pekara",
+      address: "Kolodvorska ul. 3",
+      rating: "4,5 (96)"
+    },
+
+    {
+      name: "El Pan",
+      type: "Pekara",
+      address: "Osječka ul. 20",
+      rating: "3,4 (24)"
     }
+
   ]
 
 };
