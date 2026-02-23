e2ef062c1ccdaa5fac268066378947a512ad6826
Salas-Hrastik <druzic@bak.hr>
2026-02-23 20:28:09 +0100

    Update valpovoData.js with new accommodation details

--- data/valpovoData.js
+++ data/valpovoData.js
@@ -1,322 +1,381 @@
-const valpovoData = {
-
-  /* =========================
-     SMJEŠTAJ
-     ========================= */
-
-  smještaj: [
-
+{
+  "grad": "Valpovo",
+  "drzava": "Hrvatska",
+  "izvor": "Google Maps API",
+  "azurirano": "2025",
+  "napomena": "Uključeni su smještajni objekti u gradu Valpovo i neposrednoj okolici (Belišće, Ladimirevci). Za svaki objekt dostupan je Google Maps link.",
+  "ukupno_objekata": 13,
+  "smjestaj": [
     {
-      name: "Villa Valpovo",
-      type: "Hotel",
-      category: "4 zvjezdice",
-      address: "Bana J. Jelačića 1, Valpovo"
+      "id": 1,
+      "naziv": "Hotel & Restoran Park",
+      "vrsta": "hotel",
+      "adresa": "Ul. Ive Lole Ribara 10, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6511604,
+        "lng": 18.4173152
+      },
+      "ocjena": 4.7,
+      "broj_recenzija": 1405,
+      "radno_vrijeme": {
+        "pon": "10:00–22:00",
+        "uto": "10:00–22:00",
+        "sri": "10:00–22:00",
+        "cet": "10:00–22:00",
+        "pet": "10:00–22:00",
+        "sub": "10:00–22:00",
+        "ned": "11:00–16:00"
+      },
+      "razina_cijene": "srednja (€€)",
+      "sadrzaj": [
+        "restoran",
+        "parking",
+        "klimatizacija",
+        "WiFi",
+        "moderna soba"
+      ],
+      "istaknute_recenzije": [
+        "Hrana je bila jako ukusna i u velikim porcijama! Osoblje je sjajno i ljubazno.",
+        "Odličan boravak – hrana je bila najbolja u Hrvatskoj, sobe moderne, čiste i uredne.",
+        "Fantastična večera – sve je sjajno pripremljeno, ugodan ambijent."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJy6h3UVoiXUcRnchctIY6oNA",
+      "place_id": "ChIJy6h3UVoiXUcRnchctIY6oNA"
     },
-
     {
-      name: "Valpovački Dvori",
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "Matije Gupca 95, Valpovo"
+      "id": 2,
+      "naziv": "Hotel Villa Valpovo",
+      "vrsta": "hotel",
+      "adresa": "Ul. bana Josipa Jelačića 1, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6551775,
+        "lng": 18.4266142
+      },
+      "ocjena": 4.0,
+      "broj_recenzija": 431,
+      "radno_vrijeme": null,
+      "razina_cijene": null,
+      "sadrzaj": [
+        "doručak uključen",
+        "restoran",
+        "WiFi",
+        "pogled na potok"
+      ],
+      "istaknute_recenzije": [
+        "Hotel je ok. Doručak je dobar i bogat izborom hrane.",
+        "Ugodan i mali smještaj, sobe čiste i dobro održavane. Osoblje ljubazno i od pomoći.",
+        "Kratki, ali lijep boravak. Profesionalna usluga, dočekali nas čekajući u 2 ujutro."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJ3yT2UFoiXUcRwTsCBJEhE0c",
+      "place_id": "ChIJ3yT2UFoiXUcRwTsCBJEhE0c"
     },
-
     {
-      name: 'Studio Apartman "M"',
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "Kralja Petra Krešimira IV 6, Valpovo"
+      "id": 3,
+      "naziv": "Valpovački dvori SOBE",
+      "vrsta": "sobe / pansion",
+      "adresa": "Ul. Matije Gupca 95, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6658827,
+        "lng": 18.4062756
+      },
+      "ocjena": 4.5,
+      "broj_recenzija": 149,
+      "radno_vrijeme": {
+        "pon–ned": "07:00–22:00"
+      },
+      "razina_cijene": null,
+      "sadrzaj": [
+        "doručak",
+        "prostranje sobe",
+        "domaća atmosfera",
+        "iznimno ljubazan vlasnik"
+      ],
+      "istaknute_recenzije": [
+        "Jako ugodno mjesto. Vlasnik ulaže sve srce kako bi gostima bilo ugodno – predivno!",
+        "Sobe su super prostrane i čiste. Doručak je fenomenalan, a mladić koji nas je dočekao izuzetno je ljubazan.",
+        "Fantastično!!!"
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJURbIrlsiXUcRe0grlK-_RVc",
+      "place_id": "ChIJURbIrlsiXUcRe0grlK-_RVc"
     },
-
     {
-      name: 'Studio Apartman "Baky"',
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "Ivana Gorana Kovačića 31, Valpovo"
+      "id": 4,
+      "naziv": "Apartmani Majetić",
+      "vrsta": "apartman",
+      "adresa": "Ul. Ivana Gorana Kovačića 25, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6635163,
+        "lng": 18.4184837
+      },
+      "ocjena": 4.9,
+      "broj_recenzija": 8,
+      "radno_vrijeme": null,
+      "razina_cijene": null,
+      "sadrzaj": [
+        "potpuno opremljen stan",
+        "hladnjak s hranom za dobrodošlicu",
+        "mirno okruženje",
+        "ljubazni domaćini",
+        "dječja posteljina na zahtjev"
+      ],
+      "istaknute_recenzije": [
+        "Ugodan, velik i čist apartman u lijepoj kući. Vlasnik je prijateljski i od pomoći. Sve savršeno!",
+        "Danijela i njezin muž su odlični domaćini. Apartman je savršen – pravi mirni oaza. Hladnjak bio pun, na svakom krevetu mala pozornost. Topla preporuka!",
+        "Preporučujem apartmane svakome tko cijeni mir i ljubaznu uslugu."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJre9PR2sjXUcRkdYsncjBSQU",
+      "place_id": "ChIJre9PR2sjXUcRkdYsncjBSQU"
     },
-
     {
-      name: 'Sobe za iznajmljivanje "Čičak"',
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "B.I. Mažuranića 10, Valpovo"
+      "id": 5,
+      "naziv": "StudioApartman M SelfCheckIn",
+      "vrsta": "apartman",
+      "adresa": "Ul. kralja Petra Krešimira IV 6, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6616795,
+        "lng": 18.4154192
+      },
+      "ocjena": 5.0,
+      "broj_recenzija": 13,
+      "radno_vrijeme": null,
+      "razina_cijene": null,
+      "sadrzaj": [
+        "self check-in (ključ kod sefa s kodom)",
+        "novouređen moderni enterijer",
+        "aparat za kavu",
+        "fen za kosu",
+        "glačalo",
+        "šampon i toaletni pribor",
+        "dječja posteljica na zahtjev",
+        "besplatno parkiranje",
+        "WiFi",
+        "mirna lokacija"
+      ],
+      "istaknute_recenzije": [
+        "Prekrasan apartman na odličnoj lokaciji. Čist, uredan, sa svime što bi mogao trebati. Dočekani piće dobrodošlice i dječja posteljina za bebu. Nadamo se da ćemo doći opet!",
+        "Novouređen apartman u modernom stilu. Sve je funkcionalno i lijepo uređeno. Mir i tišina – pravo mjesto za odmor.",
+        "Self check-in je jako praktičan, posebno za kasni dolazak. Besplatno parkiranje dostupno."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJ90IeaBQjXUcRsUEGfKIdgXo",
+      "place_id": "ChIJ90IeaBQjXUcRsUEGfKIdgXo"
     },
-
     {
-      name: 'Sobe "Room D"',
-      type: "Soba za iznajmljivanje",
-      category: "3 zvjezdice",
-      address: "Ulica Bana Ivana Mažuranića 10, Valpovo"
+      "id": 6,
+      "naziv": "Apartman Centar",
+      "vrsta": "apartman",
+      "adresa": "Osječka ul. 6, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6574201,
+        "lng": 18.4189767
+      },
+      "ocjena": 5.0,
+      "broj_recenzija": 3,
+      "radno_vrijeme": null,
+      "razina_cijene": null,
+      "sadrzaj": [
+        "u samom centru grada",
+        "prostoran",
+        "čist",
+        "blizina trgovina"
+      ],
+      "istaknute_recenzije": [
+        "Odličan, prostoran, savršeno čist, u samom centru, trgovine pred nosom. Da postoji veća ocjena, dala bih čistu desetku!"
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJy9wVFQAjXUcRNXp3LSM-278",
+      "place_id": "ChIJy9wVFQAjXUcRNXp3LSM-278"
     },
-
     {
-      name: 'Soba za iznajmljivanje "Draft Room"',
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "Dr. Franje Tuđmana 3, Valpovo"
+      "id": 7,
+      "naziv": "Setnik Guesthouse",
+      "vrsta": "guesthouse / sobe",
+      "adresa": "Osječka ul. 48, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6556733,
+        "lng": 18.4233701
+      },
+      "ocjena": 3.9,
+      "broj_recenzija": 34,
+      "radno_vrijeme": null,
+      "razina_cijene": "povoljno",
+      "sadrzaj": [
+        "lijep vrt",
+        "potpuno opremljena kuhinja",
+        "doručak ili večera na zahtjev",
+        "blizina centra"
+      ],
+      "istaknute_recenzije": [
+        "Vlasnica je predivna žena, dočekala nas je čak i uz kasnu rezervaciju. Sobe čiste, kupatilo opremljeno, kuhinja potpuno opremljena. Preporučujem.",
+        "Ugodna kuća s lijepim vrtom, vlasnica pruža potpuni komfor po skromnoj cijeni. U centru Valpova!",
+        "Povoljno, čisto! Marljiva domaćica."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJmYilDkQiXUcRSCdJ4VhqMtI",
+      "place_id": "ChIJmYilDkQiXUcRSCdJ4VhqMtI"
     },
-
     {
-      name: 'Soba "VaLux"',
-      type: "Objekt u domaćinstvu",
-      category: "4 zvjezdice",
-      address: "Dobriše Cesarića 58, Valpovo"
+      "id": 8,
+      "naziv": "Sobe za iznajmljivanje \"Čičak\"",
+      "vrsta": "sobe",
+      "adresa": "Ul. bana Josipa Jelačića 10, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6576080,
+        "lng": 18.4218709
+      },
+      "ocjena": 3.9,
+      "broj_recenzija": 13,
+      "radno_vrijeme": {
+        "pon–ned": "0–24h (non-stop)"
+      },
+      "razina_cijene": null,
+      "sadrzaj": [
+        "non-stop dostupno",
+        "centar grada"
+      ],
+      "istaknute_recenzije": [
+        "Uredno i čisto, vlasnik Dragan Čičak zna posao.",
+        "Autentično iskustvo.",
+        "Odlično."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJxUFY4jIjXUcR1HxKBXu2DDQ",
+      "place_id": "ChIJxUFY4jIjXUcR1HxKBXu2DDQ"
     },
-
     {
-      name: "Prenoćište Setnik",
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "Osječka 48, Valpovo"
+      "id": 9,
+      "naziv": "Sobe za iznajmljivanje M&S",
+      "vrsta": "motel / sobe",
+      "adresa": "Trg kralja Tomislava 6, 31550, Valpovo",
+      "koordinate": {
+        "lat": 45.6589139,
+        "lng": 18.4176272
+      },
+      "ocjena": null,
+      "broj_recenzija": 0,
+      "radno_vrijeme": null,
+      "razina_cijene": null,
+      "sadrzaj": [
+        "centar grada – Trg kralja Tomislava"
+      ],
+      "istaknute_recenzije": [],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJmfUuABUjXUcRy7EBlIwLe0s",
+      "place_id": "ChIJmfUuABUjXUcRy7EBlIwLe0s"
     },
-
     {
-      name: "Olive Holiday House",
-      type: "Objekt u domaćinstvu",
-      category: "5 zvjezdica",
-      address: "Matije Gupca 22, Harkanovci"
+      "id": 10,
+      "naziv": "Privatni smještaj Tolić",
+      "vrsta": "privatni smještaj / B&B",
+      "adresa": "Ul. Vladimira Nazora 2, 31550, Ladimirevci (okolica Valpova)",
+      "koordinate": {
+        "lat": 45.6229741,
+        "lng": 18.4540758
+      },
+      "ocjena": 4.7,
+      "broj_recenzija": 14,
+      "radno_vrijeme": null,
+      "razina_cijene": null,
+      "sadrzaj": [
+        "dobra vrijednost za novac",
+        "ugodni domaćini",
+        "mirno ruralno okruženje"
+      ],
+      "istaknute_recenzije": [
+        "Dobra razina smještaja za 3 zvjezdice.",
+        "Ugodni i jako ljubazni domaćini!",
+        "Uvijek savršeno!"
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJNZQsu1kfXUcRzUJQZ7HjztY",
+      "place_id": "ChIJNZQsu1kfXUcRzUJQZ7HjztY"
     },
-
     {
-      name: "Business Club",
-      type: "Objekt u domaćinstvu",
-      category: "4 zvjezdice",
-      address: "Sunčana 26, Valpovo"
+      "id": 11,
+      "naziv": "Sobe za iznajmljivanje \"Anica\"",
+      "vrsta": "sobe",
+      "adresa": "Ul. Ljudevita Gaja 17, 31551, Belišće (5 km od Valpova)",
+      "koordinate": {
+        "lat": 45.6818866,
+        "lng": 18.4080492
+      },
+      "ocjena": 4.6,
+      "broj_recenzija": 16,
+      "radno_vrijeme": {
+        "pon": "07:00–20:00",
+        "uto–ned": "08:00–20:00"
+      },
+      "razina_cijene": null,
+      "sadrzaj": [
+        "klima",
+        "udoban krevet",
+        "zajednički kupaonica",
+        "kuhinja s priborom",
+        "čisto i uredno"
+      ],
+      "istaknute_recenzije": [
+        "Gđa Ana je jako ljubazna, soba je čista i uredna, opremljena klimom, krevet udoban. Sigurno ću se vratiti i preporučiti prijateljima.",
+        "Čisto, uredno, sve što trebaš za prenoćište. Kuhinja ima sav potreban pribor, gđa Ana izuzetno ljubazna."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJKd6DUAUjXUcRhVjE7lBupiQ",
+      "place_id": "ChIJKd6DUAUjXUcRhVjE7lBupiQ"
     },
-
     {
-      name: "Apartman Nives i Apartman Lea",
-      type: "Objekt u domaćinstvu",
-      category: "3/4 zvjezdice",
-      address: "Ivana Gorana Kovačića 25, Valpovo"
+      "id": 12,
+      "naziv": "Hostel Belišće",
+      "vrsta": "hostel",
+      "adresa": "Obala Matije Ljubeka 2, 31551, Belišće (5 km od Valpova)",
+      "koordinate": {
+        "lat": 45.6891651,
+        "lng": 18.4169842
+      },
+      "ocjena": 4.6,
+      "broj_recenzija": 14,
+      "radno_vrijeme": null,
+      "razina_cijene": "povoljno",
+      "sadrzaj": [
+        "uz rijeku Dravu",
+        "lijepo uređena zgrada",
+        "popularno kod biciklista (ruta Amazonka Europe)",
+        "ugodna atmosfera"
+      ],
+      "istaknute_recenzije": [
+        "Lijepo uređena zgrada – savršena za bicikliste na turi kroz Slavoniju.",
+        "Lijepa atmosfera uz rijeku Dravu.",
+        "Predivno."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJT2mi0KgjXUcRqPuTe9oCB3M",
+      "place_id": "ChIJT2mi0KgjXUcRqPuTe9oCB3M"
     },
-
     {
-      name: "Apartman Centar",
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "Osječka ulica 6, Valpovo"
-    },
-
-    {
-      name: "Apartman Ana 1",
-      type: "Objekt u domaćinstvu",
-      category: "4 zvjezdice",
-      address: "Ivana Gundulića 23, Valpovo"
-    },
-
-    {
-      name: 'Apartman "Tea"',
-      type: "Objekt u domaćinstvu",
-      category: "4 zvjezdice",
-      address: "Učka 57, Valpovo"
-    },
-
-    {
-      name: "Privatni smještaj Tolić",
-      type: "Objekt u domaćinstvu",
-      category: "3 zvjezdice",
-      address: "Vladimira Nazora 2, Ladimirevci"
-    },
-
-    {
-      name: "M&S – Sobe za iznajmljivanje",
-      type: "Objekt u domaćinstvu",
-      category: "4 zvjezdice",
-      address: "Trg kralja Tomislava 6, Valpovo"
+      "id": 13,
+      "naziv": "Sobe Industrial",
+      "vrsta": "sobe / B&B",
+      "adresa": "Radnička ul. 1, 31551, Belišće (5 km od Valpova)",
+      "koordinate": {
+        "lat": 45.6878581,
+        "lng": 18.4069444
+      },
+      "ocjena": 4.8,
+      "broj_recenzija": 52,
+      "radno_vrijeme": null,
+      "razina_cijene": null,
+      "sadrzaj": [
+        "potpuno nove sobe",
+        "doručak i večera",
+        "iznimno čisto",
+        "osoblje prijateljsko",
+        "popularno kod biciklista (ruta Amazonka Europe – Slavonija)"
+      ],
+      "istaknute_recenzije": [
+        "Koristio sam Sobe Industrial biciklirajući rutom Amazonka Europe. Sobe su potpuno nove i super čiste. Hrana za večeru i doručak odlična. Osoblje izuzetno prijateljsko. Preporučam svim posjetiteljima Belišća.",
+        "Vrlo čiste i lijepe sobe."
+      ],
+      "google_maps_url": "https://www.google.com/maps/place/?q=place_id:ChIJL-K9N2MjXUcRbSIc1HOxXe4",
+      "place_id": "ChIJL-K9N2MjXUcRbSIc1HOxXe4"
     }
-
   ],
-
-
-  /* =========================
-     GASTRONOMIJA
-     ========================= */
-
-  gastronomija: [
-
-    /* RESTORANI */
-
-    {
-      name: "PIGment",
-      type: "Restoran",
-      address: "Trg kralja Tomislava 7",
-      rating: "5.0 (29 recenzija)",
-      description: "Burgeri, pulled pork"
-    },
-
-    {
-      name: "Restoran Jovalija",
-      type: "Restoran",
-      address: "Ul. Ive Lole Ribara 1",
-      rating: "4,9 (48)",
-      description: "Klasični restoran"
-    },
-
-    {
-      name: "Hotel & Restoran Park",
-      type: "Restoran",
-      address: "Ul. Ive Lole Ribara 10",
-      rating: "4,7 (1403)",
-      description: "Roštilj, riba"
-    },
-
-    {
-      name: "RasoPaso",
-      type: "Restoran",
-      address: "Strossmayerova ul. 2",
-      rating: "4,4 (52)"
-    },
-
-    {
-      name: "Gurman",
-      type: "Restoran",
-      address: "Ul. Augusta Šenoe 107",
-      rating: "4,0 (143)",
-      description: "Pizze, ćevapi, dostava"
-    },
-
-    /* PIZZERIA */
-
-    {
-      name: "Caffe Pizzeria HEX",
-      type: "Pizzeria",
-      address: "Trg kralja Tomislava 18",
-      rating: "4,7 (364)",
-      description: "Pizze, sladoled, kolači"
-    },
-
-    /* BRZA HRANA */
-
-    {
-      name: "Fast Food Vespera",
-      type: "Brza hrana",
-      address: "Ul. Zrinsko Frankopanska 48",
-      rating: "4,9 (118)",
-      description: "Burgeri, hot dog, sendviči"
-    },
-
-    {
-      name: "Fast Food Lana",
-      type: "Brza hrana",
-      address: "Osječka ul. 14",
-      rating: "4,0 (58)",
-      description: "Ćevapi"
-    },
-
-    /* CAFFE BAROVI */
-
-    {
-      name: "Caffe Bar Kolaž",
-      type: "Caffe bar",
-      address: "Trg kralja Tomislava 16",
-      rating: "4,8 (122)"
-    },
-
-    {
-      name: "Caffe Bar Best",
-      type: "Caffe bar",
-      address: "Trg kralja Tomislava",
-      rating: "4,7 (362)"
-    },
-
-    {
-      name: "Caffe Bar Prandau",
-      type: "Caffe bar",
-      address: "Ul. Matije Gupca 27",
-      rating: "4,8 (67)"
-    },
-
-    {
-      name: "Caffe Bar Vespera",
-      type: "Caffe bar",
-      address: "Ul. Zrinsko Frankopanska 48",
-      rating: "4,7 (105)"
-    },
-
-    {
-      name: "Cafe Barba",
-      type: "Caffe bar",
-      address: "Osječka ul. 8",
-      rating: "4,7 (109)"
-    },
-
-    {
-      name: "Caffe Bar House",
-      type: "Caffe bar",
-      address: "Ul. Braće Radić",
-      rating: "4,7 (17)"
-    },
-
-    {
-      name: "Baza",
-      type: "Caffe bar",
-      address: "Ul. kralja Petra Krešimira IV 1",
-      rating: "4.6 (276)",
-      description: "Rock atmosfera, glazba uživo"
-    },
-
-    {
-      name: "Gradska kavana Katančić",
-      type: "Kavana",
-      address: "Trg kralja Tomislava 2",
-      rating: "4.4 (88)"
-    },
-
-    {
-      name: "Caffe Bar Space",
-      type: "Pub",
-      address: "Ul. Braće Radić 9",
-      rating: "4,0 (67)"
-    },
-
-    /* NOĆNI BAROVI */
-
-    {
-      name: "Caffe Bar Passage",
-      type: "Noćni bar",
-      address: "Trg kralja Tomislava 16",
-      rating: "4.6 (317)"
-    },
-
-    {
-      name: "Caffe Night Bar City",
-      type: "Noćni bar",
-      address: "Ul. bana Ivana Mažuranića 8",
-      rating: "4,5 (98)"
-    },
-
-    {
-      name: "Cafe Bar Blue",
-      type: "Noćni bar",
-      address: "Trg kralja Tomislava 17-18",
-      rating: "4.3 (113)"
-    },
-
-    {
-      name: "Barcode Night Bar",
-      type: "Klub",
-      address: "Osječka ul. 14",
-      rating: "4.1 (103)"
-    },
-
-    /* PEKARE */
-
-    {
-      name: "Čičak d.o.o.",
-      type: "Pekara",
-      address: "Kolodvorska ul. 3",
-      rating: "4,5 (96)"
-    },
-
-    {
-      name: "El Pan",
-      type: "Pekara",
-      address: "Osječka ul. 20",
-      rating: "3,4 (24)"
-    }
-
-  ]
-
-};
-
-export default valpovoData;
+  "statistika": {
+    "hoteli": 2,
+    "apartmani": 3,
+    "sobe_pansion": 5,
+    "guesthouse_hostel": 3,
+    "prosjek_ocjene": 4.5,
+    "raspon_ocjena": "3.9 – 5.0",
+    "napomena_lokacije": "Objekti ID 11–13 nalaze se u Belišću (~5 km) i Ladimirevci (~5 km), koji su funkcionalno dio valpovačke aglomeracije"
+  }
+}
