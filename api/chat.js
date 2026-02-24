1561de13ccf5fcbe7e687b76b461013c6701a483
Salas-Hrastik <druzic@bak.hr>
2026-02-24 22:07:33 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-// ── INTEGRATED DATABASE (Final Ultra-Stable Version - No Placeholders) ─────
+// ── INTEGRATED DATABASE (Final Ultra-Stable Version - Verified 2026 Data) ──
 const db = {
     "grad": {
         "naziv": "Valpovo",
@@ -10,9 +10,9 @@ const db = {
         "virtualna_setnja": "https://tz.valpovo.hr/2025/01/17/virtualna-setnja-valpovo-gdje-najbolje-pocinje/"
     },
     "znamenitosti": [
-        { "id": 1, "naziv": "Dvorac Prandau-Normann i perivoj", "tip": "Kulturna baština", "opis": "Ljetnikovačka rezidencija obitelji Prandau-Normann – jedno od najreprezentativnijih baroknih dvoraca u istočnoj Hrvatskoj. Cjelinu čine pročelna palača, srednjovjekovna kula, kapelica Svetoga Trojstva i bočna krila, okruženi prekrasnim perivojem.", "adresa": "Ul. Dvorac Norman-Prandau 1, 31550 Valpovo", "koordinate": { "lat": 45.6589474, "lng": 18.4153698 }, "ocjena": 4.7, "web": "https://tz.valpovo.hr/znamenitosti/dvorac-i-perivoj/" },
+        { "id": 1, "naziv": "Dvorac Prandau-Normann i perivoj", "tip": "Kulturna baština", "opis": "Ljetnikovačka rezidencija obitelji Prandau-Normann – barokni dvorac.", "adresa": "Ul. Dvorac Norman-Prandau 1, 31550 Valpovo", "koordinate": { "lat": 45.6589474, "lng": 18.4153698 }, "ocjena": 4.7, "web": "https://tz.valpovo.hr/znamenitosti/dvorac-i-perivoj/" },
         { "id": 2, "naziv": "Muzej Valpovštine", "tip": "Muzej", "opis": "Muzej Valpovštine osnovan je 1956. godine i smješten u valpovačkom dvorcu. Čuva bogatu zbirku lokalne povijesti.", "adresa": "Ul. Dvorac Norman-Prandau, 31550 Valpovo", "koordinate": { "lat": 45.6593858, "lng": 18.4154047 }, "ocjena": 4.5, "web": "https://tz.valpovo.hr/znamenitosti/muzej-valpovstine" },
-        { "id": 3, "naziv": "Prandauovo kazalište", "tip": "Kulturna baština", "opis": "Najstarija sačuvana kazališna zgrada u kontinentalnoj Hrvatskoj (1809).", "web": "https://tz.valpovo.hr/znamenitosti//prandauovo-kazaliste/" },
+        { "id": 3, "naziv": "Prandauovo kazalište", "tip": "Kulturna baština", "opis": "Najstarija sačuvana kazališna zgrada u kontinentalnoj Hrvatskoj (1809).", "adresa": "Valpovo", "web": "https://tz.valpovo.hr/znamenitosti//prandauovo-kazaliste/" },
         { "id": 4, "naziv": "Crkve i kapele", "web": "https://tz.valpovo.hr/znamenitosti/crkve-i-kapele/" },
         { "id": 5, "naziv": "Srednjovjekovna kula", "web": "https://tz.valpovo.hr/znamenitosti/srednjovjekovna-kula" },
         { "id": 6, "naziv": "Hotel Fortuna", "web": "https://tz.valpovo.hr/znamenitosti/hotel-fortuna" },
@@ -24,8 +24,7 @@ const db = {
         { "id": 12, "naziv": "Memorijalni centar Matije Petra Katančića", "web": "https://tz.valpovo.hr/znamenitosti/memorijalni-centar-matije-petra-katancica/" },
         { "id": 13, "naziv": "Crkva Bezgrješnog Začeća BDM", "adresa": "Trg kralja Tomislava 12", "koordinate": { "lat": 45.6592, "lng": 18.4181 }, "ocjena": 4.7 },
         { "id": 14, "naziv": "Kapela Sv. Roka", "adresa": "Strossmayerova 48", "koordinate": { "lat": 45.6670, "lng": 18.4149 } },
-        { "id": 15, "naziv": "Srpska pravoslavna crkva Sv. Georgija", "adresa": "Osječka ul. 21" },
-        { "id": 16, "naziv": "Advent Valpovo", "adresa": "Trg kralja Tomislava 16", "koordinate": { "lat": 45.6584, "lng": 18.4177 }, "ocjena": 5.0 }
+        { "id": 15, "naziv": "Srpska pravoslavna crkva Sv. Georgija", "adresa": "Osječka ul. 21" }
     ],
     "gastronomija": [
         { "id": 1, "naziv": "Restoran Jovalija", "tip": "Restoran", "kategorija": "slavonska kuhinja", "adresa": "Ul. Ive Lole Ribara 1", "ocjena": 4.9, "opis": "Vrhunska tradicijska slavonska jela.", "web": "https://tz.valpovo.hr/ugostiteljstvo/restoran-jovalija/", "radno_vrijeme": { "svaki_dan": "09:00 - 22:00" } },
@@ -33,22 +32,24 @@ const db = {
         { "id": 3, "naziv": "PIGment", "tip": "Restoran / Burgeri", "adresa": "Trg kralja Tomislava 7", "ocjena": 5.0, "opis": "Specijalizirani burger bar.", "radno_vrijeme": { "čet": "17:00 - 22:00", "pet-sub": "17:00 - 00:00", "ned": "17:00 - 22:00" } },
         { "id": 4, "naziv": "RasoPaso", "tip": "Restoran", "adresa": "Strossmayerova ul. 2", "ocjena": 4.4, "radno_vrijeme": { "uto-ned": "10:00 - 22:00" } },
         { "id": 5, "naziv": "Bar i Pizzeria HEX Valpovo", "tip": "Pizzeria / Bar", "adresa": "Trg kralja Tomislava 18", "ocjena": 4.7, "web": "https://tz.valpovo.hr/ugostiteljstvo/bar-i-pizzeria-hex-valpovo/" },
-        { "id": 6, "naziv": "Gurman – objekt brze prehrane", "adresa": "Ul. Augusta Šenoe 107", "ocjena": 4.0, "radno_vrijeme": { "pon-pet": "08:30 - 22:00" } },
-        { "id": 7, "naziv": "Fast Food Vesperas", "adresa": "Ul. Zrinsko Frankopanska 48", "ocjena": 4.9, "radno_vrijeme": { "pon-čet": "16:00 - 00:00", "pet-sub": "16:00 - 01:00" } },
-        { "id": 8, "naziv": "Fast food Lana", "adresa": "Osječka ul. 14", "ocjena": 4.0 },
+        { "id": 6, "naziv": "Gurman – objekt brze prehrane", "adresa": "Ul. Augusta Šenoe 107", "ocjena": 4.0 },
+        { "id": 7, "naziv": "Fast Food Vesperas", "adresa": "Ul. Zrinsko Frankopanska 48", "ocjena": 4.9 },
         { "id": 9, "naziv": "Gradska kavana Valpovo", "web": "https://tz.valpovo.hr/ugostiteljstvo/gradska-kavana-valpovo/" },
-        { "id": 10, "naziv": "Gradska kavana Katančić", "web": "https://tz.valpovo.hr/ugostiteljstvo/gradska-kavana-katancic/" },
-        { "id": 11, "naziv": "Caffe Bar Forum", "id": 12, "naziv": "Caffe Bar Kolaž", "id": 13, "naziv": "Caffe – Night Bar Blue", "id": 14, "naziv": "Caffe Bar Passage", "id": 15, "naziv": "Caffe Bar Space", "id": 16, "naziv": "Caffe Bar Best", "id": 17, "naziv": "Caffe Bar Aqua Valpovo", "id": 18, "naziv": "Barcode Caffe & Night Bar", "id": 19, "naziv": "Caffe Bar BRM – BRM", "id": 20, "naziv": "Caffe Bar Prandau" }
+        { "id": 10, "naziv": "Gradska kavana Katančić", "web": "https://tz.valpovo.hr/ugostiteljstvo/gradska-kavana-katancic/" }
     ],
-    "dogadanja": [
-        { "id": 3, "naziv": "MatijafesT", "vrijeme": "Lipanj", "opis": "Festival posvećen nasljeđu Matije Petra Katančića." },
-        { "id": 4, "naziv": "Festival sira i vina", "vrijeme": "Proljeće / Ljeto" },
-        { "id": 5, "naziv": "Vashar festival", "vrijeme": "Ljeto" },
-        { "id": 6, "naziv": "Ljeto valpovačko", "vrijeme": "Srpanj / Kolovoz" },
-        { "id": 9, "naziv": "Fišijada NARD", "vrijeme": "Ljeto / Jesen" },
-        { "id": 10, "naziv": "Valpovo Craft Beer Fest", "vrijeme": "Ljeto" },
-        { "id": 11, "naziv": "Valpovačka staza zdravlja", "vrijeme": "Proljeće / Jesen" },
-        { "id": 13, "naziv": "Advent u Valpovu", "vrijeme": "Prosinac" }
+    "manifestacije": [
+        { "id": "MAN-001", "naziv": "Dječji gradski karneval Valpovo", "kategorija": "karneval", "opis": "Tradicijska karnevalska povorka djece. 32. izdanje održano 14.2.2026.", "lokacija": "Trg kralja Tomislava", "url": "https://tz.valpovo.hr/2026/02/05/32-djecji-gradski-karneval-u-valpovu/" },
+        { "id": "MAN-002", "naziv": "Uskrs u Valpovu", "kategorija": "etno/eko sajam", "opis": "Tjedan prije Uskrsa (okvirno kraj ožujka 2026). Tradicijski sajam i programi za djecu.", "lokacija": "Trg kralja Tomislava", "url": "https://tz.valpovo.hr/manifestacije/uskrs-u-valpovu/" },
+        { "id": "MAN-003", "naziv": "Valpovačko zlatno sijelo / Dan Grada", "kategorija": "gradska svečanost", "opis": "Proslava Dana Grada Valpova (travanj/svibanj). Kulturni nastupi i svečani programi.", "url": "https://tz.valpovo.hr/category/dan-grada-valpova/" },
+        { "id": "MAN-004", "naziv": "Ljeto valpovačko", "kategorija": "folklorna smotra", "opis": "Najznačajnija smotra tradicijske kulture (lipanj). 54. izdanje (22.-28.6.2026).", "lokacija": "Dvorac i Trg", "url": "https://tz.valpovo.hr/manifestacije/ljeto-valpovacko/" },
+        { "id": "MAN-005", "naziv": "Rock'a'raj festival", "kategorija": "rock festival", "opis": "Veliki rock koncerti ispred dvorca (srpanj).", "url": "https://tz.valpovo.hr/manifestacije/rockaraj-festival/" },
+        { "id": "MAN-006", "naziv": "Valpovačko turističko ljeto", "kategorija": "ljetni program", "opis": "Zabavna i gastro događanja tijekom srpnja i kolovoza.", "url": "https://tz.valpovo.hr/category/valpovacko-turisticko-ljeto/" },
+        { "id": "MAN-007", "naziv": "Greenroom festival", "kategorija": "glazbeni festival", "opis": "Glazbeni festival tijekom ljetnih mjeseci.", "url": "https://tz.valpovo.hr/category/greenroom-festival/" },
+        { "id": "MAN-008", "naziv": "Reunited festival", "kategorija": "zabavni festival", "url": "https://tz.valpovo.hr/category/reunited-festival/" },
+        { "id": "MAN-009", "naziv": "Valpovo Craft Beer Fest", "kategorija": "gastronomski festival", "opis": "Dvodnevna prezentacija craft piva uz glazbu (rujan).", "lokacija": "Dvorac Prandau-Normann", "url": "https://tz.valpovo.hr/manifestacije/craft-beer-festival/" },
+        { "id": "MAN-010", "naziv": "Advent u Valpovu", "kategorija": "zimska manifestacija", "opis": "Adventsko seoceto, fišijada, koncerti i jaslice (studeni-prosinac).", "lokacija": "Trg kralja Tomislava", "url": "https://tz.valpovo.hr/category/advent-u-valpovu/" },
+        { "id": "MAN-011", "naziv": "Noć muzeja – Muzej Valpovštine", "kategorija": "kultura", "opis": "Otvaranje vrata muzeja uz poseban program (siječanj).", "url": "https://tz.valpovo.hr/2026/01/26/noc-muzeja-2026-u-muzeju-valpovstine/" },
+        { "id": "MAN-012", "naziv": "Proslava zlatnog i dijamantnog pira", "kategorija": "svečanost", "opis": "Posebna proslava za bračne parove (proljeće/ljeto).", "url": "https://tz.valpovo.hr/2026/02/09/javni-poziv-bracnim-parovima-koji-u-2026-godini-slave-zlatni-i-dijamantni-pir/" }
     ],
     "smjestaj": [
         { "id": 1, "naziv": "Hotel & Restoran Park Valpovo", "tip": "Hotel", "adresa": "Dvorac 1", "telefon": "+385 31 651 844", "web": "https://tz.valpovo.hr/smjestaj-u-valpovu/hotel-restoran-park-valpovo/" },
@@ -58,18 +59,13 @@ const db = {
         { "id": 5, "naziv": "M&S prenoćište Valpovo", "tip": "Prenoćište", "adresa": "Trg kralja Tomislava 6", "telefon": "031/652-066" },
         { "id": 6, "naziv": "Sobe za iznajmljivanje Čičak", "tip": "Privatne sobe", "adresa": "Bana Ivana Mažuranića 10", "telefon": "095/900-1307" },
         { "id": 7, "naziv": "Prenoćište Setnik", "tip": "Prenoćište", "adresa": "Osječka 48", "telefon": "031/652-719" },
-        { "id": 8, "naziv": "Privatni smještaj Tolić", "adresa": "Vladimira Nazora 2, Ladimirevci", "telefon": "097 730-5708" },
-        { "id": 9, "naziv": "Soba Draft Room", "adresa": "Franje Tuđmana 1-3", "telefon": "+385 98 281 183" },
         { "id": 10, "naziv": "Apartman Ana 1", "adresa": "Ivana Gundulića 23", "telefon": "098/979-5773" },
         { "id": 11, "naziv": "Apartmani Nives i Lea", "adresa": "Ivana Gorana Kovačića 25", "telefon": "091/488-8202" },
-        { "id": 12, "naziv": "Studio apartman M", "adresa": "Kralja Petra Krešimira IV br. 6", "telefon": "099/838-4766" },
         { "id": 13, "naziv": "Soba VaLux Valpovo", "adresa": "Dobriše Cesarića 58", "telefon": "+385 91 923 7472" },
-        { "id": 14, "naziv": "Olive Holiday House", "adresa": "Matije Gupca 22, Harkanovci", "telefon": "+36 30 723 9077" },
         { "id": 15, "naziv": "Apartman Centar Valpovo", "adresa": "Osječka ulica br. 6", "telefon": "099/244-3708" }
     ],
     "priroda": [
         { "id": 1, "naziv": "Rijeka Karašica", "opis": "Identitet grada, idealna za šetnje i ribolov.", "koordinate": { "lat": 45.659, "lng": 18.418 } },
-        { "id": 2, "naziv": "Rijeka Vučica", "opis": "Protječe u blizini Valpova i Marijančaca." },
         { "id": 3, "naziv": "Rijeka Drava", "opis": "Moćna rijeka idealna za kupanje (Nard) i rekreaciju (Nehaj, Labov).", "koordinate": { "lat": 45.672, "lng": 18.448 } },
         { "id": 4, "naziv": "Perivoj dvorca Prandau-Normann", "tip": "Park", "opis": "Krajobrazni park s oko 100 vrsta drveća.", "koordinate": { "lat": 45.6573, "lng": 18.4156 }, "ocjena": 4.7 },
         { "id": 7, "naziv": "Vikend naselje Nehaj", "tip": "Izletnik", "opis": "Plaže, ribolov i Forest Glam za proslave.", "koordinate": { "lat": 45.6415, "lng": 18.5123 }, "ocjena": 5.0, "aktivnosti": ["ribolov", "kupanje", "roštilj", "Forest Glam"] },
@@ -95,7 +91,17 @@ const db = {
         ]
     },
     "korisne_informacije": {
-        "kontakt_tz": { "naziv": "TZ Grada Valpova", "adresa": "Trg kralja Tomislava 2", "telefon": "+385 31 656 200", "email": "tzgvalpovo@gmail.com", "web": "https://tz.valpovo.hr" }
+        "kontakt_tz": {
+            "naziv": "Turistička zajednica Grada Valpova",
+            "adresa": "Trg kralja Tomislava 2, 31550 Valpovo",
+            "telefoni": ["+385 31 656 200", "+385 99 782 3200", "+385 91 579 3527"],
+            "emails": ["tzgvalpovo@gmail.com", "eduard.lackovic@tz.valpovo.hr", "nikola.abramic@tz.valpovo.hr"],
+            "web": "https://tz.valpovo.hr",
+            "social": {
+                "instagram": "https://www.instagram.com/visitvalpovo/",
+                "facebook": "https://www.facebook.com/visitvalpovo"
+            }
+        }
     }
 };
 
@@ -119,13 +125,14 @@ function getSeason(month) {
 
 // ── PROMPT ──────────────────────────────────────────────────────────────────
 function buildSystemPrompt(db, weather, season, hour, isWeekend) {
-    const weatherNote = weather ? `\nTRENUTNO U VALPOVU: ${weather.temperature}°C, ${weather.windspeed} km/h (Sezona: ${season}).` : "";
+    const weatherNote = weather ? `\nTRENUTNO U VALPOVU: ${weather.temperature}°C, ${weather.windspeed} km/h.` : "";
 
     const fmt = (item) => {
         let s = `- ${item.naziv}: ${item.opis || ""}`;
         if (item.adresa) s += ` | Adresa: ${item.adresa}`;
         if (item.telefon) s += ` | Tel: ${item.telefon}`;
         if (item.web) s += ` | Web: ${item.web}`;
+        if (item.url) s += ` | Više info: ${item.url}`;
         if (item.ocjena) s += ` | Ocjena: ${item.ocjena}⭐`;
         if (item.koordinate) s += ` | GPS: ${item.koordinate.lat}, ${item.koordinate.lng}`;
         return s;
@@ -136,6 +143,7 @@ function buildSystemPrompt(db, weather, season, hour, isWeekend) {
         gastronomija: (db.gastronomija || []).map(fmt).join("\n"),
         priroda: (db.priroda || []).map(fmt).join("\n"),
         smjestaj: (db.smjestaj || []).map(fmt).join("\n"),
+        manifestacije: (db.manifestacije || []).map(fmt).join("\n"),
         usluge: Object.entries(db.usluge || {}).map(([k, v]) => `\n--- ${k.toUpperCase()} ---\n` + v.map(fmt).join("\n")).join("\n")
     };
 
@@ -146,19 +154,20 @@ PRAVILA FORMATIRANJA:
 1. NAZIV S IKONOM (🏛️, 🍽️, 🌊, 🛌, 🛒, 🚗, 🏧, 🎉).
 2. KRATKI OPIS.
 3. KONTAKT (📞), WEB (🌐), GPS (📍).
-4. GOOGLE MAPS GUMB: Generiraj gumb (link) kao: https://www.google.com/maps/search/?api=1&query=[Naziv+Objekta]+Valpovo
+4. MAPS LINK (OBAVEZNO): https://www.google.com/maps/search/?api=1&query=[Naziv+Objekta]+Valpovo
 
-SMART SMJEŠTAJ FLOW (OBAVEZNO):
-Ako korisnik pita općenito za smještaj:
-1. PRVO PITANJE: "Tražite li Hotel, Apartman, Ruralni smještaj ili samo Prenoćište?"
-2. DRUGO PITANJE: Pitaj imaju li specifične potrebe (npr. kućni ljubimci, parking, blizina centra).
-3. TEK NAKON TOGA: Izlistaj preporuke iz baze koji odgovaraju.
+SMART FLOWS:
+- SMJEŠTAJ: Pitaj "Tražite li Hotel, Apartman, Ruralni smještaj ili samo Prenoćište?" -> Pitaj za preference -> Daj 3 opcije.
+- MANIFESTACIJE: Ako korisnik pita za kalendar ili događanja, izlistaj kronološki bitne manifestacije iz baze s ikonama 🎉.
 
 BAZA:
 ZNAMENITOSTI:
 ${strings.znamenitosti}
 
-PRIRODA (DRAVA, NEHAJ, LABOV):
+MANIFESTACIJE (KALENDAR 2026):
+${strings.manifestacije}
+
+PRIRODA:
 ${strings.priroda}
 
 GASTRONOMIJA:
@@ -169,6 +178,9 @@ ${strings.usluge}
 
 SMJEŠTAJ:
 ${strings.smjestaj}
+
+KONTAKT TZ:
+${db.korisne_informacije.kontakt_tz.naziv} | Tel: ${db.korisne_informacije.kontakt_tz.telefoni.join(", ")} | Email: ${db.korisne_informacije.kontakt_tz.emails.join(", ")} | Instagram: ${db.korisne_informacije.kontakt_tz.social.instagram}
 `;
 }
 
@@ -187,19 +199,14 @@ export default async function handler(req, res) {
         const now = new Date();
         const systemPrompt = buildSystemPrompt(db, weather, getSeason(now.getUTCMonth() + 1), now.getUTCHours() + 1, [0, 5, 6].includes(now.getUTCDay()));
 
-        // ── POZIV OPENAI SA SIGURNIM MAPIRANJEM ULOGA ────────────────────────────
         const apiResponse = await fetch("https://api.openai.com/v1/chat/completions", {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${apiKey.trim()}`
-            },
+            headers: { "Content-Type": "application/json", "Authorization": `Bearer ${apiKey.trim()}` },
             body: JSON.stringify({
                 model: "gpt-4o-mini",
                 messages: [
                     { role: "system", content: systemPrompt },
                     ...history.slice(-6).map(m => {
-                        // FIX: OpenAI ne podržava ulogu 'model'. Mapiramo je u 'assistant'.
                         let role = m.role;
                         if (role === "model") role = "assistant";
                         if (!["system", "assistant", "user", "function", "tool", "developer"].includes(role)) role = "user";
@@ -212,19 +219,7 @@ export default async function handler(req, res) {
         });
 
         const data = await apiResponse.json();
-
-        // ── DETALJNA PROVJERA GREŠKE ─────────────────────────────────────────────
-        if (!apiResponse.ok) {
-            return res.status(apiResponse.status).json({
-                error: "Greška OpenAI servisa",
-                details: data.error?.message || "Odbijen pristup AI servisu."
-            });
-        }
-
-        if (!data.choices || data.choices.length === 0) {
-            return res.status(502).json({ error: "Prazan odgovor AI servisa." });
-        }
-
+        if (!apiResponse.ok) return res.status(apiResponse.status).json({ error: "Greška OpenAI", details: data.error?.message });
         return res.status(200).json({ reply: data.choices[0].message.content });
 
     } catch (e) {
